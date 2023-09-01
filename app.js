48548e6ceb6fd99a6e87d0a997a995f9291d4e40
Mekebib Ergudo <ergudomekebib95@gmail.com>
2023-08-31 22:35:28 -0400

    finalize the projec

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const form = document.getElementById("form");
 const emailInput = document.getElementById("emailInput");
 const errorSpan = document.querySelector(".error");
-const input = document.getElementsByTagName("input");
+const successMessage = document.getElementById("successMessage");
+const container = document.querySelector(".container"); // Select the container element
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault(); // Prevent the default form submission
 	const emailValue = emailInput.value;
-	// console.log(emailValue);
+
 	if (emailValue === "" || !emailValue.includes("@")) {
 		errorSpan.textContent = "Please enter a valid email address";
-		return;
+	} else {
+		container.style.display = "none"; // Hide the container
+		successMessage.style.display = "block";
+		errorSpan.textContent = "";
 	}
-	// Clear the error message if the email is valid
-	errorSpan.textContent = "";
-
-	// Perform further actions (e.g., form submission) if needed
 });
